2726d952c22e13b4ae39402310ce6e259c1cb270
Juan Pablo Calao <jcalaop@gmail.com>
2020-07-30 21:44:50 -0500

    node and webpack boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = function webpackConfig(env) {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ];
+
+  if (env.NODE_ENV === 'production') {
+    plugins.push(new CleanWebpackPlugin());
+  }
+
+  return {
+    entry: './src/index.js',
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'js/[name].js',
+      publicPath: '/',
+    },
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
+    mode: env.NODE_ENV,
+    devServer: {
+      compress: true,
+      port: 3000,
+      historyApiFallback: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)?$/,
+          exclude: '/node_modules',
+          use: {
+            loader: 'babel-loader',
+          },
+        },
+        {
+          test: /\.(jpg|png|gif|svg)$/,
+          use: {
+            loader: 'url-loader',
+            options: {
+              limit: 1000000,
+              fallback: 'file-loader',
+              name: 'images/[name].[hash].[ext]',
+            },
+          },
+        },
+      ],
+    },
+    plugins,
+  };
+};

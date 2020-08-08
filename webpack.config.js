64e691acf23dd6d7ff3d449e42dfe0d2a277cf1e
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-08 10:59:03 -0500

    add sass support

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = function webpackConfig(env) {
   const plugins = [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ];
+      template: './public/index.html'
+    })
+  ]
 
   if (env.NODE_ENV === 'production') {
-    plugins.push(new CleanWebpackPlugin());
+    plugins.push(new CleanWebpackPlugin())
   }
 
   return {
@@ -18,16 +20,16 @@ module.exports = function webpackConfig(env) {
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'js/[name].js',
-      publicPath: '/',
+      publicPath: '/'
     },
     resolve: {
-      extensions: ['.js', '.jsx'],
+      extensions: ['.js', '.jsx']
     },
     mode: env.NODE_ENV,
     devServer: {
       compress: true,
       port: 3000,
-      historyApiFallback: true,
+      historyApiFallback: true
     },
     module: {
       rules: [
@@ -35,8 +37,35 @@ module.exports = function webpackConfig(env) {
           test: /\.(js|jsx)?$/,
           exclude: '/node_modules',
           use: {
-            loader: 'babel-loader',
-          },
+            loader: 'babel-loader'
+          }
+        },
+        {
+          test: /\.module.scss?$/,
+          loader: [
+            'style-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                modules: true
+              }
+            },
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.scss$/,
+          exclude: /\.module.(scss)$/,
+          loader: [
+            {
+              loader:
+                env.NODE_ENV === 'development'
+                  ? 'style-loader'
+                  : MiniCssExtractPlugin.loader
+            },
+            'css-loader',
+            'sass-loader'
+          ]
         },
         {
           test: /\.(jpg|png|gif|svg)$/,
@@ -45,12 +74,15 @@ module.exports = function webpackConfig(env) {
             options: {
               limit: 1000000,
               fallback: 'file-loader',
-              name: 'images/[name].[hash].[ext]',
-            },
-          },
-        },
-      ],
+              name: 'images/[name].[hash].[ext]'
+            }
+          }
+        }
+      ]
     },
     plugins,
-  };
-};
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})]
+    }
+  }
+}

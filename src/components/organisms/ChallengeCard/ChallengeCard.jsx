a618ae56ae4f8f1d0577f6786b18d60b39c34bb0
Juan Camilo Moreno <juancamilomg21@gmail.com>
2020-08-09 13:34:41 -0500

    Add Challenge Card

---
+++ src/components/organisms/ChallengeCard/ChallengeCard.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import styles from './ChallengeCard.module.scss'
+
+export function ChallengeCard({ children }) {
+  return (
+    <div className={styles.ChallengeCard}>
+      <div className={styles.ChallengeCard__content}>
+        <figure>
+          <img
+            src="https://res.cloudinary.com/dz209s6jk/image/upload/q_auto,w_700/Challenges/jve24exh4iwonznfndfc.jpg"
+            alt="ChallengeCard"
+          />
+        </figure>
+        <div className={styles['ChallengeCard__content--info']}>
+          <h2>Aweasome Title</h2>
+          <span>Level: Intermediate</span>
+          {/* {children} */}
+        </div>
+        <h3>HTML | CSS | JS</h3>
+      </div>
+      {children}
+    </div>
+  )
+}

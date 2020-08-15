60bb03164fb04c2ef3f2442e89ec6b10795b33bd
Juan Pablo Calao <jcalaop@gmail.com>
2020-08-15 12:36:11 -0500

    add challenge details section

--- src/components/molecules/ChallengeDetails/ChallengeDetails.jsx
+++ src/components/molecules/ChallengeDetails/ChallengeDetails.jsx
@@ -1,5 +1,24 @@
 import React from 'react'
+import styles from './ChallengeDetails.module.scss'
 
-export function ChallengeDetails() {
-  return <h3>ChallengeDetails component</h3>
+export function ChallengeDetails({ description, tips }) {
+  return (
+    <section className={styles.ChallengeDetails}>
+      <div className={styles.ChallengeDetails__details}>
+        <h1>Challenge Details</h1>
+        <p>{description}</p>
+      </div>
+      <div className={styles.ChallengeDetails__recommendations}>
+        <h1>Recommendations | tips</h1>
+        <ol>
+          {tips.map((tip, idx) => (
+            // eslint-disable-next-line react/no-array-index-key
+            <li key={`recommendation-tip-${idx}`} value={idx + 1}>
+              {tip}
+            </li>
+          ))}
+        </ol>
+      </div>
+    </section>
+  )
 }
